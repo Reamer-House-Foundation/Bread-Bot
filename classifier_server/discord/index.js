6216cb1f95741950c6bbde21b9b4960515ddf9a7
xylafur <james@jkesley.com>
2023-04-02 16:27:04 -0500

    Clean up servers and containers

--- classifier_server/discord/index.js
+++ classifier_server/discord/index.js
@@ -23,28 +23,32 @@ client.on('messageCreate', async (message) => {
     try {
         // Get the first image attachment
         const attachment = message.attachments.first();
-        
+
         // Download the image and convert it to a Buffer
         const response = await axios.get(attachment.url, { responseType: 'arraybuffer' });
         const imageBuffer = Buffer.from(response.data, 'binary');
-        
+
         // Create a FormData object and append the image
         const formData = new FormData();
         formData.append('image', imageBuffer, attachment.name);
-        
+
 
         // TODO: Make this settable using env variables
         // Send a POST request with the image using axios
-        const result = await axios.post('http://web:8000/predict', formData, {
+        const result = await axios.post('http://classifier:8000/predict', formData, {
             headers: {
                 'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
             },
         });
-        
+
         if (result.data.class == 'bread') {
-            message.reply('This image is bread!');
+            message.reply(`This image is bread!\n\n` +
+                          `Bread Confidence:\t${result.data.bread_confidence}\n`
+                          `Not-Bread Confidence:\t${result.data.not_bread_confidence}`);
         } else {
-            message.reply('This image is not bread!');
+            message.reply(`This image is bread!\n\n` +
+                          `Bread Confidence:\t${result.data.bread_confidence}\n`
+                          `Not-Bread Confidence:\t${result.data.not_bread_confidence}`);
         }
     } catch (error) {
         console.error(error);
